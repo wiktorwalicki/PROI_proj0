004a126b5d1c975730f7871a210da42bff36da17
Wiktor Walicki <wwalicki@stud.elka.pw.edu.pl>
2018-04-12 09:22:22 +0200

    zamiana kolejnosci

--- klasy.cpp
+++ klasy.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <cstring>
 #include "klasy.h"
 
 using namespace std;
+
+string order (string str)
+{
+    int k=0;
+    string strReturn;
+    for (int i = str.length()-1; i>-1; i--)
+    {
+        strReturn+=str[i];
+        k++;
+    }
+    return strReturn;
+}
 
 void human::add()
 {
     cout << "Write name:" << endl;
     cin >> name;
+    name=order(name);
     cout << "Write last name: " << endl;
     cin >> lastName;
+    lastName=order(lastName);
     cout << "Write nickname: " << endl;
     cin >> nick;
+    nick=order(nick);
 }
 
 void human::read()
